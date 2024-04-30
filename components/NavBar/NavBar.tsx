9042967cfebdba716d5c2b833c415d9912645a07
Gokulnath Anand <gokulnathanand23@gmail.com>
2024-04-30 02:27:23 -0400

    removed unwanted project tabs

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -51,16 +51,19 @@ const sideBarItems: SideBarItems = {
     { label: "Grants", href: "/grants", icon: Landmark },
     { label: "Publications", href: "/publications", icon: Newspaper },
     { label: "Projects", href: "/projects", icon: FolderClosed },
-    { label: "Downloads", icon: Download, sub: [{label: "Software", href: "/downloads/software"}, {label: "Data", href: "/downloads/data"}] },
+    {
+      label: "Downloads",
+      icon: Download,
+      sub: [
+        { label: "Software", href: "/downloads/software" },
+        { label: "Data", href: "/downloads/data" },
+      ],
+    },
     { label: "Useful Resources", href: "/useful-resources", icon: Wrench },
   ],
 };
 
 export function NavBar() {
-  const isDesktop = useMediaQuery("(min-width: 640px)", {
-    initializeWithValue: false,
-  });
-
   return (
     <div className="bg-[#00274C] flex justify-between lg:h-36 h-24 w-screen drop-shadow-md">
       <div className=" text-[#FFCB05] text-xl text-center p-5 content-center">
@@ -98,40 +101,11 @@ export function NavBar() {
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
-                    <NavigationMenuLink asChild>
-                      <a
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
-                      >
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components that you can copy and
-                          paste into your apps. Accessible. Customizable. Open
-                          Source.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
+              <Link href="/projects" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Projects
+                </NavigationMenuLink>
+              </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
               <NavigationMenuTrigger>Downloads</NavigationMenuTrigger>
